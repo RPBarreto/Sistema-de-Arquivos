14735c088577013252add9a6d415c31425d8cb3a
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-05-16 19:21:27 -0300

    updates for root checking

--- changedir.py
+++ changedir.py
@@ -2,9 +2,25 @@
 import os
 def changedir(nome):
     path = os.getcwd()
-    #totalpath= os.path.join(path,nome)
+    root = 'root'
+    last = path.split('\\')
+    print(last)
+    print(path)
     try:
-        os.chdir(nome)
+        if (len(nome) >= 2):
+            if(nome[0] == '.' and nome[1]=='.' and len(nome)<3 and last[-1]!=root):
+                os.chdir(nome)
+            elif(nome[0] != '.' and nome[1]!='.'):
+                os.chdir(nome)
+            else:
+                raise Exception("raiz")
+        elif(nome[0] == '.'):
+                print(path)
+        else:
+            os.chdir(nome)
+            
+    except Exception as e:
+        print("Erro:" % e)
     except OSError:
         print("arquivo não encontrado")
     else:
