1a8ee773a0208e87c054f0bbd6698f7502880715
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-05-15 21:42:28 -0300

    Update removefile.py

--- removefile.py
+++ removefile.py
@@ -1,6 +1,8 @@
 import os
 def removefile(nome):
-    if os.path.exists(nome):
-        os.remove(nome)
+    path = os.getcwd()
+    totalpath= os.path.join(path,nome)
+    if os.path.exists(totalpath):
+        os.remove(totalpath)
     else:
         print("Arquivo não Existe")
