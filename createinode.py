982691e20b3c9828562538074a26445c95b67355
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-05-16 20:19:18 -0300

    Create createinode.py

---
+++ createinode.py
@@ -0,0 +1,18 @@
+from filesize import filesize
+import os
+def createinodefile(nome):
+    path = os.getcwd()
+    
+    totalpath= os.path.join(path,nome)
+    nome = 'inode' + nome + 'txt'
+    totalpath2= os.path.join(path,nome)
+
+    if os.path.exists(totalpath2):
+        print("Inode já Existe")
+    else:
+        f=open(totalpath2,"w")
+        print("Inode criado")
+        f.write(totalpath+'\n')
+        f.write(filesize(totalpath)+'\n')
+        #loop e verificar pra ver quantos blocos pega
+        f.close
