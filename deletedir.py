77e7eee5c1efb05ec0251ccbd88eb3c3fa4a3488
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-05-15 21:32:41 -0300

    Create deletedir.py

---
+++ deletedir.py
@@ -0,0 +1,10 @@
+import os
+def deletedir(nome):
+    path = os.getcwd()
+    totalpath= os.path.join(path,nome)
+    try:
+        os.rmdir(totalpath)
+    except OSError:
+        print("Falha em deletar a pasta" % totalpath)
+    else:
+        print("pasta deletada com sucesso")
