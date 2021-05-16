093f837bbbb564e2d1840103c78a473c0b83e0e5
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-05-15 21:22:42 -0300

    Create makedir.py

---
+++ makedir.py
@@ -0,0 +1,10 @@
+import os
+def makedir(nome):
+    path = os.getcwd()
+    totalpath= os.path.join(path,nome)
+    try:
+        os.mkdir(totalpath)
+    except OSError:
+        print("Falha na criação da pasta" % totalpath)
+    else:
+        print("pasta criada com sucesso")
