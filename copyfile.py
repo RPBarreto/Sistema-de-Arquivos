fb5fa0db3baf72ce8aef17d72440094a46f4ddcb
Renan Paredes Barreto <Renan.barretoo@hotmail.com>
2021-05-15 21:53:44 -0300

    Create copyfile.py

---
+++ copyfile.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+def copyfile(nome,alvo):
+    path = os.getcwd()
+    totalpath= os.path.join(path,nome)
+    totalpath2= os.path.join(path,alvo)
+    try:
+        shutil.copy(totalpath,totalpath2)
+    except IOError as error:
+        print("Falha na copia do arquivo" % error)
+    else:
+        print("arquivo copiado com sucesso")
