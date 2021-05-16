9111b40744ed92e88a6e8253e304911ce2cd04b8
Davi Lopes Ribeiro <foxpc18@gmail.com>
2021-05-16 00:35:37 -0300

    Update tratamento.py
    Update command

--- tratamento.py
+++ tratamento.py
@@ -9,37 +9,38 @@ from makedir import *
 from readfile import *
 from removefile import *
 from renamefiledir import *
+import re
 
 
 def tratamento(input):
     comando = input
     comandoRaw = comando
-    comando.split()
+    command = comando.split()
     comandoRaw.split("")  # nao funciona
 
-    if (comando[0] == "SHUTDOWN"):
+    if (comando[0] == 'SHUTDOWN'):
         return(True)
-    if (comando[0] == "touch"):
-        createfile(comando[1])
-    if (comando[0] == "rm"):
-        removefile(comando[1])
-    if (comando[0] == "echo"):
+    if (comando[0] == 'touch'):
+        createfile(command[1])
+    if (comando[0] == 'rm'):
+        removefile(command[1])
+    if (comando[0] == 'echo'):
         text = re.search('"(.*)"', input)
-        appendfile(comando[-1], text)
+        appendfile(command[-1], text)
     # malditas aspas
-    if (comando[0] == "cat"):
-        readfile(comando[1])
-    if (comando[0] == "cp"):
-        copyfile(comando[1], comando[2])
-    if (comando[0] == "mv"):
-        renamefiledir(comando[1])
-    if (comando[0] == "mkdir"):
-        makedir(comando[1])
-    if (comando[0] == "rmdir"):
-        deletedir(comando[1])
-    if (comando[0] == "cd"):
-        changedir(comando[1])
-    if (comando[0] == "."):
-        changedir(comando[1])
-    if (comando[0] == ".."):
-        changedir(comando[1])
+    if (comando[0] == 'cat'):
+        readfile(command[1])
+    if (comando[0] == 'cp'):
+        copyfile(command[1], command[2])
+    if (comando[0] == 'mv'):
+        renamefiledir(command[1])
+    if (comando[0] == 'mkdir'):
+        makedir(command[1])
+    if (comando[0] == 'rmdir'):
+        deletedir(command[1])
+    if (comando[0] == 'cd'):
+        changedir(command[1])
+    if (comando[0] == '.'):
+        changedir(command[1])
+    if (comando[0] == '..'):
+        changedir(command[1])
