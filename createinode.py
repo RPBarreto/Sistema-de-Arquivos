987ebcdb9bcb641862cf8704e19b5c0e706ae180
Davi Lopes Ribeiro <foxpc18@gmail.com>
2021-05-16 23:37:24 -0300

    information update file
    information update file

--- createinode.py
+++ createinode.py
@@ -1,20 +1,24 @@
 from filesize import filesize
 import os
+
+
 def createinodefile(nome):
     path = os.getcwd()
     print(path)
     print(nome)
-    totalpath= os.path.join(path,nome)
-    nome = 'inode' + nome #+ 'txt'
-    totalpath2= os.path.join(path,nome)
+    totalpath = os.path.join(path, nome)
+    nome = 'inode' + nome  # + 'txt'
+    totalpath2 = os.path.join(path, nome)
 
     if os.path.exists(totalpath2):
         print("Inode já Existe")
     else:
-        f=open(totalpath2,"w")
+        f = open(totalpath2, "w")
         print("Inode criado")
-        blocks = (int(filesize(totalpath)/4096)) + (filesize(totalpath)%4096 > 0)
-
-        f.writelines([(totalpath+'\n'),(str(blocks) + '\n')])
+        blocks = (int(filesize(totalpath)/4096)) + \
+            (filesize(totalpath) % 4096 > 0)
+        print("Tamanho do arquivo " + str(filesize(totalpath)))
+        print("Blocos ocupados " + str(blocks))
+        f.writelines([(totalpath+'\n'), (str(blocks) + '\n')])
         f.write(str(os.stat(totalpath))+'\n')
         f.close
--- updateinode.py
+++ updateinode.py
@@ -12,14 +12,16 @@ def updateinodefile(nome):
     if os.path.exists(totalpath2):
 
         f = (open(totalpath2, "w"))
-        blocks = (int(filesize(totalpath)/4096)) + (filesize(totalpath)%4096 > 0)
-        f.writelines([(totalpath+'\n'),(str(blocks) + '\n')])
+        blocks = (int(filesize(totalpath)/4096)) + \
+            (filesize(totalpath) % 4096 > 0)
+        f.writelines([(totalpath+'\n'), (str(blocks) + '\n')])
         f.write(str(os.stat(totalpath))+'\n')
         print("Inode Atualizado")
+        print("Tamanho do arquivo " + str(filesize(totalpath)))
+        print("Blocos ocupados " + str(blocks))
         f.close
     else:
 
         print("Inode não Existe")
-
 
         # loop e verificar pra ver quantos blocos pega
